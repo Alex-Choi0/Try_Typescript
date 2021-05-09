336f49a422f0e7dd4cf7fa4254a9ace2f6e83756
Alex-Choi <choijeaho86@gmail.com>
2021-05-10 07:40:10 +0900

    add another one

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Your code goes here...');
+function add(n1, n2, showResult) {
+    if (showResult === true) {
+        console.log(n1 + n2);
+        return;
+    }
+    else {
+        return n1 + n2;
+    }
+}
+var number1 = 5;
+var number2 = 2.8;
+var printResult = true;
+var result = add(number1, number2, printResult);
